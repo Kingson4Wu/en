34658e271bf0e6da33f3ec3141ec224470f91adf
kingson4wu <kingson4wu@gmail.com>
2025-12-16 19:48:48 +0800

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://kingson4wu.github.io',
+	base: '/en/',
 	integrations: [mdx(), sitemap()],
 });
